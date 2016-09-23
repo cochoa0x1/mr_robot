f7c7c3d7d35298b966c9bbb3f3d98a56f4e9e62c
Chris Ochoa <cochoa0x1@gmail.com>
2016-09-23 12:20:10 -0400

    initial commit

---
+++ robot.py
@@ -0,0 +1,47 @@
+import serial
+from struct import pack, unpack
+import time
+
+s = serial.Serial('/dev/ttyUSB1',115200)
+s.timeout =1
+s.write_timeout =1
+
+#sudo chmod a+rw /dev/ttyUSB0 
+
+def update_motors(v_left, v_right):
+
+	d_left = v_left >= 0
+	d_right = v_right >=0
+	s_left = abs(int(v_left))
+	s_right = abs(int(v_right))
+
+
+	motor_packet = pack('>c?B?B','M',d_left,s_left,d_right,s_right)
+
+	s.reset_output_buffer()
+	s.write(motor_packet)
+
+
+def read_ultrasonic():
+	v= s.readline()
+	s.reset_input_buffer()
+	return float(v.strip('\r\n'))
+
+
+def test_loop(t):
+
+	while(1):
+		try:
+			v = read_ultrasonic()
+			print 'ultrasonic: ', v
+
+			if v < 10.0:
+				update_motors(100,100)
+			else:
+				update_motors(-50,100)
+
+		except:
+			print 'error'
+
+		time.sleep(t)
+
---
+++ robot_firmware/robot_firmware.ino
@@ -0,0 +1,113 @@
+
+#define echo_pin 8
+#define echo_trigger_pin 7
+
+#define speed_l 9
+#define fwd_l 11
+#define rev_l 10
+
+#define speed_r 6
+#define fwd_r 5
+#define rev_r 4
+
+
+unsigned long time;
+unsigned long sensor_time;
+
+struct motorPacket {
+  bool left_positive;
+  int speed_left;
+  bool right_positive;
+  int speed_right;
+};
+
+
+void setup() {
+
+  pinMode(echo_pin, INPUT);
+  pinMode(echo_trigger_pin, OUTPUT);
+
+  //left motor
+  pinMode(speed_l,OUTPUT);
+  pinMode(fwd_l,OUTPUT);
+  pinMode(rev_l,OUTPUT);
+  
+  Serial.begin(115200);
+
+  time=millis();
+  sensor_time =0;
+}
+
+
+double echo_read(double ec_pin, double tr_pin){
+  //performs an echo read and returns the results
+  
+  digitalWrite(tr_pin, LOW);
+  delayMicroseconds(2);
+
+  //send a 10 ms pulse
+  digitalWrite(tr_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(tr_pin, LOW);
+  
+  return pulseIn(ec_pin, HIGH)/58.2;
+}
+
+
+void loop() {
+
+  //shutdown the motors if we haven't received a motor packet in the last 500 milliseconds
+  if(millis()-time > 200){
+      digitalWrite(fwd_l,LOW);
+      digitalWrite(rev_l,LOW);
+      digitalWrite(fwd_r,LOW);
+      digitalWrite(rev_r,LOW);
+  }
+//
+//   //read the ultrasonic
+if(millis() - sensor_time > 50){
+    double d = echo_read(echo_pin, echo_trigger_pin);
+    Serial.println(d);
+    sensor_time = millis();
+ }
+
+     
+  if(Serial.available() >= 5){
+    
+    char cmd = Serial.read();
+
+    if (cmd == 'M'){
+
+      time = millis();
+
+      //run the motors
+      motorPacket  m;
+      int len= sizeof(m);
+      char buff[len];
+      
+      for(int i =0; i< len; i++){
+        buff[i] = Serial.read();
+      }
+      
+      memcpy(&m,&buff,len);
+
+      Serial.flush();
+
+      analogWrite(speed_l, m.speed_left);
+       analogWrite(speed_r, m.speed_right);
+
+      if( m.left_positive){
+        digitalWrite(fwd_l,HIGH);
+      }else{
+        digitalWrite(rev_l,HIGH);
+      }
+
+      if( m.right_positive){
+        digitalWrite(fwd_r,HIGH);
+      }else{
+        digitalWrite(rev_r,HIGH);
+      }
+   }
+  }
+
+}
